dd9b68aeeeb03e9993cb6df9e63a14119cccdd44
Anil Chapagain <anilchapagain68@gmail.com>
2021-09-20 13:44:32 +0530

    added auth

--- src/controllers/buyeers-contact.controller.ts
+++ src/controllers/buyeers-contact.controller.ts
@@ -1,30 +1,25 @@
+import {authenticate} from '@loopback/authentication';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
 import {Buyerscontact} from '../models';
 import {BuyerscontactRepository} from '../repositories';
-
+@authenticate("jwt")
 export class BuyeersContactController {
   constructor(
     @repository(BuyerscontactRepository)
-    public buyerscontactRepository : BuyerscontactRepository,
-  ) {}
+    public buyerscontactRepository: BuyerscontactRepository,
+  ) { }
 
   @post('/buyerscontacts')
   @response(200, {
