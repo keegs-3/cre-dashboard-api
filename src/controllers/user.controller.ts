45a25ddb2b0fa1cb2eaea94763feddc9f2d0f322
Anil Chapagain <anilchapagain68@gmail.com>
2021-11-17 17:48:00 +0530

    schema available for Dev and Demo

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -123,7 +123,7 @@ export class CReUserController {
   ): Promise<any> {
 
     const data = await this.usersRepository.dataSource.execute(`
-    select * from cre.users where "email" = '${email}'
+    select * from ${this.DB_SCHEMA}.users where "email" = '${email}'
     `);
     console.log(data);
     if (data.length < 1) {
@@ -164,7 +164,7 @@ SET   resetkey= '${resetkey}' where email = '${email}'
       // Preview only available when sending through an Ethereal account
       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
       // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-      return 'Successfully sent mail';
+      return 'Successfully Emailed';
     }
 
 
