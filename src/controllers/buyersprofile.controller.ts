2ff1d39cace16fdd83d4a8e51ea10f61cb6d4c62
Anil Chapagain <anilchapagain68@gmail.com>
2022-01-21 13:28:30 +0530

    buyers_profile

---
+++ src/controllers/buyersprofile.controller.ts
@@ -0,0 +1,62 @@
+import {
+  repository
+} from '@loopback/repository';
+import {get, param, response} from '@loopback/rest';
+import {BuyerscontactRepository} from '../repositories';
+// @authenticate("jwt")
+export class BuyeersProfileController {
+  constructor(
+    @repository(BuyerscontactRepository)
+    public buyerscontactRepository: BuyerscontactRepository,
+  ) { }
+  DB_SCHEMA = process.env.DB_SCHEMA
+  @get('/buyersprofile')
+  @response(200, {
+    description: 'Array of Buyerscontact model instances'
+
+  })
+  async find(
+    @param.query.string('segment') segment?: string,
+    @param.query.string('state') state?: string,
+  ): Promise<any> {
+
+    if (segment !== '' && segment !== undefined
+      && state !== '' && state !== undefined) {
+
+
+      const seg = segment.split(',');
+      const locaq = "'" + seg.join("','") + "'";
+      const st = state.split(',');
+      const stq = "'" + st.join("','") + "'";
+      const sql = await this.buyerscontactRepository.dataSource.execute(`
+      select  count(owner)as "total_owners", sum(total_property_owned) as "total_property_owned", avg(avg_monetary) as "average Dollar value"
+       from ${this.DB_SCHEMA}.tgt_owner_profiles top where owner_segment in (${locaq}) and owner_state in (${stq})
+
+      `);
+      return sql;
+    }
+  }
+
+  @get('/canaly')
+  @response(200, {
+    description: 'Array of Buyerscontact model instances'
+
+  })
+  async analy(
+    @param.query.string('property_id') property_id?: string,
+  ): Promise<any> {
+
+    if (property_id !== '' && property_id !== undefined) {
+
+
+
+      const sql = await this.buyerscontactRepository.dataSource.execute(`
+      select  *
+       from ${this.DB_SCHEMA}.tgt_properties_metrics_new top where property_id = '${property_id}'
+
+      `);
+      return sql;
+    }
+  }
+
+}
