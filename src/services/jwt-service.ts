60ab74ae88c03c113ab657c2dd3ef29815f63967
Anil Chapagain <anilchapagain68@gmail.com>
2023-04-19 16:29:01 +0530

    create user in service

--- src/services/jwt-service.ts
+++ src/services/jwt-service.ts
@@ -46,8 +46,8 @@ export class JWTService {
     try {
       const decryptedToken = await verifyAsync(token, this.jwtSecret);
       userProfile = Object.assign(
-        {[securityId]: '', id: '', name: ''},
-        {[securityId]: decryptedToken.id, id: decryptedToken.id, name: decryptedToken.name}
+        {[securityId]: '', id: '', name: '', email : '', role: ''},
+        {[securityId]: decryptedToken.id, id: decryptedToken.id, name: decryptedToken.name,email: decryptedToken.email, role: decryptedToken.role}
       );
     }
     catch (err) {
