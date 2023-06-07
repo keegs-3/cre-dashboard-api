d05dcb4b53421fe39386c9cfbbd6b062ff570f9d
Anil Chapagain <anilchapagain68@gmail.com>
2023-06-07 20:28:12 +0530

    auth

--- src/controllers/leads.controller.ts
+++ src/controllers/leads.controller.ts
@@ -5,7 +5,8 @@
 import {repository} from '@loopback/repository';
 import {HttpErrors, get, param, post, requestBody, response} from '@loopback/rest';
 import {LeadsRepository} from '../repositories';
-// @authenticate("jwt")
+import {authenticate} from '@loopback/authentication';
+@authenticate("jwt")
 export class LeadsController {
   constructor(
     @repository(LeadsRepository)
