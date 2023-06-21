e4c91393b294b5ac6e0b04b0ded407c9735fe3f5
Anil Chapagain <anilchapagain68@gmail.com>
2023-06-21 09:56:15 +0530

    controllers solution

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,12 +1,12 @@
 // export * from './ping.controller';
 export * from './buyeers-contact.controller';
-export * from './buyersprofile.controller';
-export * from './feedback.controller';
+// export * from './buyersprofile.controller';
+// export * from './feedback.controller';
 export * from './leads.controller';
 export * from './market-summary.controller';
-export * from './nedlapi.controller';
-export * from './notification.controller';
-export * from './roles.controller';
+// export * from './nedlapi.controller';
+// export * from './notification.controller';
+// export * from './roles.controller';
 // export * from './sessionuser.controller';
 export * from './status.controller';
 export * from './user.controller';
@@ -16,6 +16,6 @@ export * from './usersession.controller';
 
 
 
-export * from './userloginlocation.controller';
+// export * from './userloginlocation.controller';
 export * from './usersactions.controller';
 export * from './userreview.controller';
