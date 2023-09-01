94478608a6df1661bdd25ff838428286299b67f4
Anil Chapagain <anilchapagain68@gmail.com>
2023-09-01 12:16:51 +0530

    changes

--- src/controllers/leads.controller.ts
+++ src/controllers/leads.controller.ts
@@ -7,9 +7,7 @@ import {
   HttpErrors,
   get,
   param,
-  post,
-  requestBody,
-  response,
+  response
 } from '@loopback/rest';
 import {LeadsRepository} from '../repositories';
 // @authenticate("jwt")
@@ -453,6 +451,15 @@ limit 100 offset ${offset}
     @param.query.string('county') county?: string,
     @param.query.number('punits') punits?: number,
     @param.query.number('punite') punite?: number,
+    @param.query.number('ocs') ocs?: number,
+    @param.query.number('oce') oce?: number,
+    @param.query.number('rrs') rrs?: number,
+    @param.query.number('rre') rre?: number,
+    @param.query.string('la') la?: string,
+    @param.query.string('owner') owner?: string,
+    @param.query.string('segement') segment?: string,
+    @param.query.number('ytms') ytms?: number,
+     @param.query.number('ytme') ytme?: number,
     @param.query.number('offset', {default: 0}) offset?: number,
   ): Promise<any> {
     let marq: any = '';
@@ -460,6 +467,8 @@ limit 100 offset ${offset}
     let smarq: any = '';
     let cityc: any = '';
     let addc: any = '';
+    let own: any = '';
+    let seg: any = '';
       const mmar = market?.split(',');
       mmarq = "'" + mmar?.join("','") + "'";
       const smar = submarket?.split(',');
@@ -470,12 +479,22 @@ limit 100 offset ${offset}
       cityc = "'" + cit?.join("','") + "'";
       const add = county?.split(',');
       addc= "'" + add?.join("','") + "'";
+      const ow = owner?.split(',');
+      own= "'" + ow?.join("','") + "'";
+      const se = segment?.split(',');
+      seg= "'" + se?.join("','") + "'";
 let allState='' ;
 let allCity='';
 let allCounty='';
 let allMArket='';
 let allSMArket='';
 let allPunit='';
+let allOcr='';
+let allRr='';
+let allLa='';
+let allOwner='';
+let allSeg='';
+let allYtms='';
 
     if (
       state !== '' &&
@@ -521,6 +540,64 @@ let allPunit='';
     ) {
       allPunit = ` and units_count between ${punits} and ${punite}`;
     }
+    if (
+      ocs !== null &&
+      ocs !== undefined &&
+      oce !== null &&
+      oce !== undefined
+
+    ) {
+      allOcr = ` and units_count between ${ocs} and ${oce}`;
+    }
+    if (
+      rrs !== null &&
+      rrs !== undefined &&
+      rre !== null &&
+      rre !== undefined
+
+    ) {
+      allRr = ` and units_count between ${ocs} and ${oce}`;
+    }
+
+    if (
+      ytms !== null &&
+      ytms !== undefined &&
+      ytme !== null &&
+      ytme !== undefined
+
+    ) {
+      allYtms = `and years_to_mature  between ${ytms} and ${ytme}`;
+    }
+    if (
+      la !== '' &&
+      la !== undefined
+
+
+    ) {
+if (la==='No'){
+
+  allLa=`and loan_maturity_date is  null`;
+
+}
+else if (la==='Yes'){
+allLa=`and loan_maturity_date is not null `;
+}
+
+    }
+    if (
+      owner !== '' &&
+      owner !== undefined
+
+    ) {
+      allOwner = `  AND (owner_name IN(${own}))`;
+    }
+    if (
+      segment !== '' &&
+      segment !== undefined
+
+    ) {
+      allSeg = `  AND (owner_segment IN(${seg}))`;
+    }
 
 
 
@@ -532,7 +609,14 @@ let allPunit='';
                     ${allCity}
                     ${allCounty}
                     ${allMArket}
-                    ${allSMArket}${allPunit}
+                    ${allSMArket}
+                    ${allPunit}
+                    ${allOcr}${allRr}
+                    ${allLa}
+                    ${allOwner}
+                    ${allSeg}
+                    ${allYtms}
+                    limit 100 offset ${offset}
                   `;
                   const all = await this.leadsRepository.dataSource.execute(count)
 return all;
