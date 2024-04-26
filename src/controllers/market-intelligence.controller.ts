6678e0d6e6d24d482f119a27e77ea253ba952042
Anil Chapagain <anilchapagain68@gmail.com>
2024-04-26 16:04:22 +0530

    market intelligence changes

---
+++ src/controllers/market-intelligence.controller.ts
@@ -0,0 +1,37 @@
+// Uncomment these imports to begin using these cool features!
+
+import {repository} from '@loopback/repository';
+import {get, response} from '@loopback/rest';
+import {UserRepository} from '../repositories';
+
+// import {inject} from '@loopback/core';
+
+
+export class MarketIntelligenceController {
+  constructor(
+    @repository(UserRepository)
+    public userRepository: UserRepository,
+  ) {}
+  DB_SCHEMA = process.env.DB_SCHEMA;
+  @get('/marketIntelligence/liveFeedsTicker')
+  @response(200, {
+    description: 'Array of Live Feeds Tickers',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+        },
+      },
+    },
+  })
+  async liveFeeds(): Promise<JSON> {
+    const sql = await this.userRepository.dataSource.execute(
+      `
+    SELECT * from ${this.DB_SCHEMA}.vw_recorder limit 20
+
+    `,
+    );
+
+    return sql;
+  }
+}
