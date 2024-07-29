974507e9ece917b59a2e28e2161c05944de22e70
Anil Chapagain <anilchapagain68@gmail.com>
2024-07-29 17:39:10 +0530

    insertedOn

--- src/controllers/leads.controller.ts
+++ src/controllers/leads.controller.ts
@@ -141,8 +141,11 @@ console.log({user,subs})
             where ln.subs_id = '${subs_id}'))`;
           }
           const s = `
-          SELECT l.*
+          SELECT l.*,ln.inserted_on
           FROM nedl_model.lead_gen l
+           join ${this.DB_SCHEMA}.app_leads_notes ln
+                on l.nedl_property_id_pk = ln.property_id
+
           where
            (lead_type IN (${propenq}))
            and l.nedl_property_id_pk not in (select distinct property_id from ${this.DB_SCHEMA}.app_leads_status ls where ls.org = ${org} and ls.subs_id = ${subs_id} )
@@ -200,7 +203,10 @@ console.log({user,subs})
           }
 
           const s = `
-        SELECT l.* FROM nedl_model.lead_gen l
+        SELECT l.*,ln.inserted_on FROM nedl_model.lead_gen l
+         join ${this.DB_SCHEMA}.app_leads_notes ln
+                on l.nedl_property_id_pk = ln.property_id
+
         WHERE
         (lead_type IN (${propenq}))
         And (l.nedl_property_id_pk not in
