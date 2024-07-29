ed2e6d8df767fa2f994ba1b14f844d7254245154
Anil Chapagain <anilchapagain68@gmail.com>
2024-07-29 15:58:08 +0530

    testing status

--- src/controllers/leads.controller.ts
+++ src/controllers/leads.controller.ts
@@ -287,15 +287,19 @@ console.log({user,subs})
         }
         let myllist = '';
         if (mylist === 'yes') {
-          myllist = `and subquery.nedl_property_id_pk in (select distinct property_id from ${this.DB_SCHEMA}.app_leads_status where subs_id = ${subs_id}
-and userid = '${userid}'
-and insert_date = (select max(insert_date) from ${this.DB_SCHEMA}.app_leads_status where subs_id = ${subs_id}
-and userid = '${userid}' ))
+          myllist = `and ls.insert_date =
+          (select max(insert_date) from ${this.DB_SCHEMA}.app_leads_status
+           where subs_id = ${subs_id}
+           and userid = '${userid}'
+           and property_id = subquery.nedl_property_id_pk
+           )
                `;
         }
         if (mylist === 'no' || mylist === '' || mylist === undefined) {
-          myllist = `and subquery.nedl_property_id_pk in (select distinct property_id from ${this.DB_SCHEMA}.app_leads_status where subs_id = ${subs_id}
-and insert_date = (select max(insert_date) from ${this.DB_SCHEMA}.app_leads_status where subs_id = ${subs_id}))
+          myllist = `and ls.insert_date =
+          (select max(insert_date) from ${this.DB_SCHEMA}.app_leads_status
+           where subs_id = ${subs_id}
+           and property_id = subquery.nedl_property_id_pk
                `;
         }
         const s = `
