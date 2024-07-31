70423499e4ccfb7822a426567928bee555b7fac8
Anil Chapagain <anilchapagain68@gmail.com>
2024-07-31 14:41:34 +0530

    leads ageing

--- src/controllers/market-intelligence.controller.ts
+++ src/controllers/market-intelligence.controller.ts
@@ -1,7 +1,7 @@
 // Uncomment these imports to begin using these cool features!
 
 import {repository} from '@loopback/repository';
-import {get, response} from '@loopback/rest';
+import {get, param, response} from '@loopback/rest';
 import {UserRepository} from '../repositories';
 
 // import {inject} from '@loopback/core';
@@ -76,5 +76,54 @@ ORDER BY to_date(recording_month, 'YYYY-MM-DD') DESC;
     );
 
     return sql;
+  }
+
+  @get('/marketIntelligence/leadsAgeing')
+  @response(200, {
+    description: 'Array of Leads Ageing',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+        },
+      },
+    },
+  })
+  async ageing(
+    @param.query.string('date') date?: string,
+    @param.query.string('state') state?: string,
+  ): Promise<any> {
+    let statesd = '';
+    if (state !== '' && state !== undefined) {
+      statesd = `AND situs_state = '${state}'`;
+    }
+
+    // Query to get count
+    const countQuery = `
+    SELECT COUNT(*)
+    FROM ${this.DB_SCHEMA}.leads_aging la
+    WHERE la.year_of_analysis = $1
+    ${statesd}
+  `;
+    const count = await this.userRepository.dataSource.execute(countQuery, [
+      date,
+    ]);
+
+    // Query to get ageing data grouped by month
+    const ageingQuery = `
+    SELECT DATE_TRUNC('month', la2.deals_closed) AS month, COUNT(*)
+    FROM ${this.DB_SCHEMA}.leads_aging la2
+    WHERE la2.deals_closed BETWEEN
+          (timestamp $1 - INTERVAL '6 months') AND
+          (timestamp $1 - INTERVAL '1 month')
+          ${statesd}
+    GROUP BY DATE_TRUNC('month', la2.deals_closed)
+    ORDER BY month;
+  `;
+    const ageing = await this.userRepository.dataSource.execute(ageingQuery, [
+      date,
+    ]);
+
+    return {count, ageing};
   }
 }
