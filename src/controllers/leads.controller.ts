ff74565dd33d6e9f3b252758c10575b83b1f2bf8
Anil Chapagain <anilchapagain68@gmail.com>
2024-08-07 16:09:06 +0530

    check for issues

--- src/controllers/leads.controller.ts
+++ src/controllers/leads.controller.ts
@@ -204,7 +204,7 @@ console.log({user,subs})
           }
 
           const s = `
-        SELECT distinct on (l.nedl_property_id_pk) ln.inserted_on,l.* FROM nedl_model.lead_gen l
+        SELECT distinct on (l.nedl_property_id_pk) (select max(ln.inserted_on)as inserted_on from ${this.DB_SCHEMA}.app_leads_notes where ln.property_id = l.nedl_property_id_pk),l.* FROM nedl_model.lead_gen l
          left join ${this.DB_SCHEMA}.app_leads_notes ln
                 on l.nedl_property_id_pk = ln.property_id
         WHERE
@@ -214,11 +214,12 @@ console.log({user,subs})
          where ls.subs_id = ${subs_id}))
         ${ow}${pr}${yb}${pu}${allMSA}
         ORDER BY
+        l.nedl_property_id_pk,
         CASE
         WHEN (SELECT MAX(lnotes.inserted_on) FROM ${this.DB_SCHEMA}.app_leads_notes lnotes WHERE lnotes.property_id = l.nedl_property_id_pk and lnotes.subs_id = ${subs_id}) IS NULL THEN 2
         ELSE 1
         END,
-l.nedl_property_id_pk,
+
         insert_date_time desc,
         CASE lead_type
         WHEN 'Hot' THEN 1
