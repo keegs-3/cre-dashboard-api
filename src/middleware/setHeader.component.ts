51720c8582e182c2011bc9dc6d444c50c15cf421
Anil Chapagain <anilchapagain68@gmail.com>
2024-08-26 12:55:32 +0530

    iouio

--- src/middleware/setHeader.component.ts
+++ src/middleware/setHeader.component.ts
@@ -1,18 +1,18 @@
-import {RestServer, RestServerConfig} from '@loopback/rest';
-import {inject} from '@loopback/core';
+// import {RestServer, RestServerConfig} from '@loopback/rest';
+// import {inject} from '@loopback/core';
 
-export class MyRestServer extends RestServer {
-  constructor(@inject('rest.server.config') config: RestServerConfig) {
-    super(config);
+// export class MyRestServer extends RestServer {
+//   constructor(@inject('rest.server.config') config: RestServerConfig) {
+//     super(config);
 
-    this.configureHeaders();
-  }
+//     this.configureHeaders();
+//   }
 
-  private configureHeaders() {
-    this.handler = async (req, res, next) => {
-      res.setHeader('X-Custom-Header', 'YourValue');
-      // Call the original handler
-      return await super.handler(req, res, next);
-    };
-  }
-}
+//   private configureHeaders() {
+//     this.handler = async (req, res, next) => {
+//       res.setHeader('X-Custom-Header', 'YourValue');
+//       // Call the original handler
+//       return await super.handler(req, res, next);
+//     };
+//   }
+// }
